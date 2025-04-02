1d3ea56e36a0f4c179390ea4ed7ae20a0914fc6b
zahra <zahramoneeb83@gmail.com>
2025-04-02 20:44:00 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let navLinks = document.getElementById("navLinks");
+
+function hideMenu(){
+    navLinks.style.right = "0";
+
+};
+
+function showMenu(){
+    navLinks.style.right = "-200px";
+
+
+};
